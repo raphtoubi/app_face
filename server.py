5de3ab8d546718de8820f726b5ae885b6cac56a6
raphtoubi <43609010+raphtoubi@users.noreply.github.com>
2019-03-21 19:20:42 +0200

    Update server.py

--- server.py
+++ server.py
@@ -15,7 +15,7 @@ path = Path(__file__).parent
 
 app = Starlette()
 app.add_middleware(CORSMiddleware, allow_origins=['*'], allow_headers=['X-Requested-With', 'Content-Type'])
-app.mount('/static', StaticFiles(directory='app/static'))
+app.mount('/static', StaticFiles(directory='static'))
 
 async def download_file(url, dest):
     if dest.exists(): return
@@ -39,6 +39,26 @@ async def setup_learner():
 
 loop = asyncio.get_event_loop()
 tasks = [asyncio.ensure_future(setup_learner())]
+learn = loop.run_until_complete(asyncio.gather(*tasks))[0]
+loop.close()
+
+@app.route('/')
+def index(request):
+    html = path/'view'/'index.html'
+    return HTMLResponse(html.open().read())
+
+@app.route('/analyze', methods=['POST'])
+async def analyze(request):
+    data = await request.form()
+    img_bytes = await (data['file'].read())
+    img = open_image(BytesIO(img_bytes))
+    return JSONResponse({'result': learn.predict(img)[0]})
+
+if __name__ == '__main__':
+    if 'serve' in sys.argv: uvicorn.run(app, host='0.0.0.0', port=8888)
+
+
+
 learn = loop.run_until_complete(asyncio.gather(*tasks))[0]
 loop.close()
 
